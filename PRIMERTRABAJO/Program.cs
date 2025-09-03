47bb95f78a9daa64be0a2b6f4fa2a2e6b85ce0cf
master1 <hansell22082002@gmail.com>
2025-09-03 10:30:49 -0500

    segundo

--- PRIMERTRABAJO/Program.cs
+++ PRIMERTRABAJO/Program.cs
@@ -10,6 +10,16 @@ namespace PRIMERTRABAJO
     {
         static void Main(string[] args)
         {
+            Console.Write("Hola, ¿cual es tu nombre?: ");
+            string nombre = Console.ReadLine();
+
+            Console.Write("¿Cual es tu edad?: ");
+            int edad = Convert.ToInt32(Console.ReadLine());
+            
+            Console.Write("¿Cual es tu estatura?: ");
+            decimal estatura = Convert.ToDecimal(Console.ReadLine());
+
+            Console.WriteLine($"mi nombre es {nombre}, mi edad es {edad} y mi estatura es {estatura:N2} ");
         }
     }
 }
